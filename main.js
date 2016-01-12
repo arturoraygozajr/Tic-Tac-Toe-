a95dbbfc74ceec0f0aade9fe039a908d019be83f
Arturo Raygoza <arturoraygozajr@gmail.com>
2016-01-11 23:34:34 -0800

    Fixed over writing. Updated css.

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 (function(){
-console.log("hey");
+  var turnOne = true
+
+//console.log("hey");
 $(document).ready(function(){
 
 	$('.box').click(boxClicked)
 	function boxClicked(){
-		console.log("clicked");
-	}
+		//console.log("clicked");
+		if(turnOne){
+			console.log("x");
+      if ($(this).text() === ""){
+       turnOne = false;
+       $(this).text("X");
+       $('p').text("Player O");
+     }
+
+   } else{
+     console.log("o");
+     if ($(this).text() === ""){
+      turnOne = true;
+      $(this).text("O");
+      $('p').text("Player X");
+    }
+  }
+
+}
+
 
 
 
